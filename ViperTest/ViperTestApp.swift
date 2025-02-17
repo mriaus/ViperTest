4a56a281ce130931a9cdf3096316c46e07b4ccf7
mriaus <55255215+mriaus@users.noreply.github.com>
2025-02-17 20:01:31 +0100

    Initial Commit

---
+++ ViperTest/ViperTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViperTestApp.swift
+//  ViperTest
+//
+//  Created by Marcos on 17/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ViperTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
